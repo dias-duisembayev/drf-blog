f626da19b38abf820e5a1882e7f6214412a779f2
dias_duisembayev <dias.duisembayev@gmail.com>
2020-12-12 21:38:11 +0600

    Sinle blog can be accessed only by authenticated users

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from .models import Post
 from .serializers import PostSerializer
@@ -11,6 +11,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly, permissions.IsAuthenticated,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
