21479746addb4feff2909dc6d9f52ee640a769b6
dias_duisembayev <dias.duisembayev@gmail.com>
2020-12-12 21:27:11 +0600

    Secret key issues resovled

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 3rd party
+    # 3rd party apps
     'rest_framework',
     # Local
     'posts.apps.PostsConfig',
